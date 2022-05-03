0421fd5df591a3cc55596b944046d6f6e9938ff3
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2022-05-03 21:23:46 +0200

    Update with links

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 import { Tueet } from "./Tueet";
 import { NewTweetForm } from "./NewTweetForm";
+import { useParams } from "react-router-dom";
 
-export function Home() {
+export function Home(props) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [tueet, setTueet] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
     const load = async () => {
       setLoading(true);
       try {
         setTueet(
-          await (await fetch("http://localhost:8000/tweet/random/")).json()
+          await (
+            await fetch(`http://localhost:8000/tweet/${id ? id : "random"}/`)
+          ).json()
         );
       } catch (e) {
         setError(true);
@@ -22,7 +26,7 @@ export function Home() {
       console.log(tueet);
     };
     load();
-  }, []);
+  }, [id]);
 
   if (error) {
     return <h1>oops 😅</h1>;
